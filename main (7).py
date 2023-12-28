1a6768dccefd1132f83c5b4e7dab1ddda6f2d12c
ChristinaK9 <146163944+ChristinaK9@users.noreply.github.com>
2023-12-28 19:27:24 +0300

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,14 @@
+print("Стороны:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+P = a + b + c 
+p = (a + b + c) / 2
+S = √(p * (p — a) * (p — b) * (p — c))
+     if a + b > c and a + c > b and b + c > a:
+        print("Треугольник существует")
+    else:
+        print("Треугольник не существует")
+        print("P")
+        print("S")
+      
